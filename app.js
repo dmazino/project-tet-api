85eb0f88f9015c263283d7c8d2918eb515edd214
Dadamuga Mazino <destructicus.lives@gmail.com>
2020-04-29 02:57:42 +0100

    minor test for json response

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hey there I\'m running express server!\n')
-})
+
+    let response = {
+        name: 'Dadamuga Mazino',
+        age: 18, location: 'Nazareth',
+        message: "Hey there I'm running express server!"
+    }
+
+    res.send(response);
+});
 
 app.listen(3000)
