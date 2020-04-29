cb78906d1a536acd41b43527d77e8bf03fe078c4
Dadamuga Mazino <destructicus.lives@gmail.com>
2020-04-29 03:11:35 +0100

    added API call and data pass/merge

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 app.get('/', function (req, res) {
 
-    let response = {
+    let blob = {
         name: 'Dadamuga Mazino',
         age: 18, location: 'Nazareth',
         message: "Hey there I'm running express server!"
     }
 
-    res.send(response);
+    axios.get('https://api.synthcity.io/synthcity/v1/stats')
+        .then(function (response) {
+            // handle success
+            console.log(response.data)
+
+            res.send({...response.data, ...blob});
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error)
+            res.send('something went wrong');
+        })
+
 });
 
 app.listen(3000)
